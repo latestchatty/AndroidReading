26d16db191fa259c56168180b143c4c662c35f42
cjhodge <19308384+cjhodge@users.noreply.github.com>
2025-01-15 08:06:00 -0700

    Firebase Config
    Firebase Config

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -6,6 +6,8 @@ plugins {
     alias(libs.plugins.kotlin.android)
     alias(libs.plugins.kotlin.compose)
     id("org.jetbrains.kotlin.plugin.serialization")
+    id("com.google.gms.google-services")
+    id("com.google.firebase.appdistribution")
 }
 
 android {
@@ -98,6 +100,8 @@ dependencies {
     implementation("com.halilibo.compose-richtext:richtext-ui-material:0.17.0")
 
     implementation("io.coil-kt:coil-gif:2.5.0")
+
+    implementation(platform("com.google.firebase:firebase-bom:33.7.0"))
 
     // For Android ViewModel and LiveData
     //implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:2.6.2")
